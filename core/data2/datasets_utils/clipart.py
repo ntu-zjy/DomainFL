9facab2cbda49cb9648dfb47d7fcc0bbb850ad74
ntu-zjy <jzhang149@e.ntu.edu.sg>
2024-04-24 12:04:55 +0800

    update

--- core/data2/datasets_utils/clipart.py
+++ core/data2/datasets_utils/clipart.py
@@ -17,5 +17,11 @@ class Clipart:
         self.test_loader = DataLoader(self.test_dataset, batch_size=batch_size, num_workers=num_workers, pin_memory=True)
 
         class_to_idx = self.train_dataset.class_to_idx
-        self.classnames = [c.replace('_', ' ') for c in class_to_idx.keys()]
+        # self.classnames = [c.replace('_', ' ') for c in class_to_idx.keys()]
+
+        self.update_classnames()
+
+    def update_classnames(self):
+        self.classnames = [c.replace('_', ' ') for c in self.train_dataset.class_to_idx.keys()]
+
 
