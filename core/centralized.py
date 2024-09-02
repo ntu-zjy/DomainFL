0bbdd0fee6817a843c15dcf22ea1d537e99a7c0c
ntu-zjy <jzhang149@e.ntu.edu.sg>
2024-09-02 19:18:35 +0800

    update

--- core/centralized.py
+++ core/centralized.py
@@ -4,7 +4,7 @@ import copy
 import time
 import torch
 import argparse
-from utils.get_data import data, office
+from utils.get_data import domainnet
 from utils.get_data import get_data
 from utils.data_utils import build_subset, concat_datasets, split_train_and_val
 from utils.server import Server
@@ -31,8 +31,8 @@ def run(args):
     cls_heads = []
     for id, data_name in enumerate(dataset):
         init_image_encoder = copy.deepcopy(server.image_encoder)
-        cd = get_data(data_name, server.train_preprocess, server.val_preprocess, f'./{args.dataset}/{data_name}', args.batch_size, args.num_workers)
-        cd = build_subset(cd, args.subset_size) 
+        cd = get_data(data_name, server.train_preprocess, server.val_preprocess, args.batch_size, args.num_workers)
+        cd = build_subset(cd, args.subset_size)
         cd = split_train_and_val(cd)
         cds.append(cd)
         cls_head = server.generate_cls_head(cd, data_name)
@@ -112,7 +112,7 @@ def run(args):
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser(description='DomainFL')
-    parser.add_argument('-d','--dataset', type=str, default='data', help='Dataset name')
+    parser.add_argument('-d','--dataset', type=str, default='domainnet', help='Dataset name')
     parser.add_argument('-m','--model', type=str, default='CLIP', help='Model name')
     parser.add_argument('-ss','--subset_size', type=int, default=100, help='Subset size')
     parser.add_argument('-ien','--image_encoder_name', type=str, default='ViT-B-32', help='Image encoder name')
