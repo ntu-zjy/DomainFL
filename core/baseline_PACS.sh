3241d1ca4cbdf90125142eb9358ea23048814fea
ntu-zjy <jzhang149@e.ntu.edu.sg>
2024-09-12 01:46:31 +0000

    update mixed

---
+++ core/baseline_PACS.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+# Define constants
+
+bs=32
+algo=("local" "zeroshot" "fedavg" "fedprox" "fedditto" "fedmoon" "fedproto" "fedavgDBE")
+ss=7
+gr=200
+did=0
+ien="convnext_base"
+data=PACS
+
+# python fedavg.py -bs 32 -gr 200 -did 0 -ien "ViT-B-32" -d domainnet -ss 50
+for a in "${algo[@]}"; do
+    python "$a.py" -bs $bs -gr $gr -did $did -ien "$ien" -d $data -ss $ss
+done
+
+
+
--- core/models/CLIP.py
+++ core/models/CLIP.py
@@ -1,18 +1,15 @@
 import torch
 import copy
 import open_clip
-import clip
 import torch.nn as nn
 
 # https://github.com/mlfoundations/open_clip/blob/main/docs/openclip_results.csv
-d = {'RN50':'yfcc15m',
-    'RN101': 'cc12m',
+d = {
     'ViT-B-32': 'laion2b_s34b_b79k',
     'ViT-B-16': 'laion2b_s34b_b88k',
     'ViT-L-14': 'laion2b_s32b_b82k',
     'convnext_base': 'laion400m_s13b_b51k',
-    'nllb-clip-base': 'v1',
-    "RN50-quickgelu": 'cc12m'}
+    }
 
 class Adapter(nn.Module):
     def __init__(self, c_in, reduction=4, bias=False):
