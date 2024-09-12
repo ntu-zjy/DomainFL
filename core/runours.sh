d4a855f02c724c0dafd550aa55507fa539c26079
ntu-zjy <jzhang149@e.ntu.edu.sg>
2024-09-12 04:45:52 +0000

    update zjy

--- core/runours.sh
+++ core/runours.sh
@@ -12,7 +12,7 @@ sram=("cluster" "random")
 ratio=(0.1 0.2 0.3 0.4 0.5 0.6 0.7 0.8 0.9)
 # ratio=(0.5 0.6 0.7 0.8 0.9)
 
-# python "$algo.py" -bs $bs -gr $gr -did $did -ien $ien -d $data -ss $ss -sram "average" -sra 1
+python "$algo.py" -bs $bs -gr $gr -did $did -ien $ien -d $data -ss $ss -sram "average" -sra 1
 
 for m in "${sram[@]}"; do
     for r in "${ratio[@]}"; do
